2770d37442ea0cf927152e9e500af9c649ada94f
Gustavo Schmaltz <guuhschmaltz@gmail.com>
2020-09-08 23:09:06 -0300

    first commit

---
+++ js/principal.js
@@ -0,0 +1,12 @@
+var titleEl = document.querySelector('.titulo');
+titleEl.textContent = 'Gustavo Nutricionista';
+
+var paciente = document.querySelector('#primeiro-paciente');
+var pesoEl = paciente.querySelector('.info-peso');
+var peso = pesoEl.textContent;
+
+var paciente = document.querySelector('#primeiro-paciente');
+var alturaEl = paciente.querySelector('.info-altura');
+var altura = alturaEl.textContent;
+
+console.log(altura);
