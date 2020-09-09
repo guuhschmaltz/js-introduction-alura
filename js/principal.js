907b196444164fe0d42949efe7f9b2c576a5253f
Gustavo Schmaltz <guuhschmaltz@gmail.com>
2020-09-09 00:01:23 -0300

    adicionado validações de IMC

--- js/principal.js
+++ js/principal.js
@@ -1,12 +1,40 @@
 var titleEl = document.querySelector('.titulo');
 titleEl.textContent = 'Gustavo Nutricionista';
 
-var paciente = document.querySelector('#primeiro-paciente');
-var pesoEl = paciente.querySelector('.info-peso');
-var peso = pesoEl.textContent;
 
-var paciente = document.querySelector('#primeiro-paciente');
-var alturaEl = paciente.querySelector('.info-altura');
-var altura = alturaEl.textContent;
+var pacientes = document.querySelectorAll('.paciente');
 
-console.log(altura);
+for(i = 0; i < pacientes.length; i++) {
+
+    var peso = pacientes[i].querySelector('.info-peso').textContent;
+    var altura = pacientes[i].querySelector('.info-altura').textContent;
+    var imcEl = pacientes[i].querySelector('.info-imc');
+    
+    var pesoEhValido = true;
+    if (peso <= 0 || peso >= 250)
+    {
+        pesoEhValido = false;
+        imcEl.textContent = 'Peso inválido';
+        //pacientes[i].style.color = 'red'; Ao invés de modificarmos o CSS pelo JavaScript, devemos atribuir uma classe ao elemento
+        pacientes[i].classList.add('paciente-invalido');
+    }
+    
+    var alturaEhValida = true;
+    if (altura <= 0 || altura >= 3.00)
+    {
+        alturaEhValida = false;
+        imcEl.textContent = 'Altura inválida';
+        //pacientes[i].style.color = 'red'; Ao invés de modificarmos o CSS pelo JavaScript, devemos atribuir uma classe ao elemento
+        pacientes[i].classList.add('paciente-invalido');
+    }
+    if (pesoEhValido === true && alturaEhValida === true)
+    {
+       var imc = peso / (altura * altura);
+       imcEl.textContent = imc.toFixed(2);
+    }
+}
+
+
+
+
+
